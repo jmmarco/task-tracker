2c3832880a796e84502e5013e09600106afa9304
Juan Marco <jmmarco@gmail.com>
2024-03-21 17:59:08 +0100

    Add Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+import { ButtonHTMLAttributes, forwardRef } from "react";
+import { cn } from "../lib/utils";
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, ...props }, ref) => {
+    return <button className={cn(className)} ref={ref} {...props} />;
+  },
+);
+
+Button.displayName = "Button";
+
+export default Button;
