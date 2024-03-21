a8633cacf3ce082b5c789ac2664443057cc5ec11
Juan Marco <jmmarco@gmail.com>
2024-03-21 17:57:17 +0100

    Add useGetTasks hook for fetching tasks

---
+++ src/hooks/useGetTasks.ts
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import axios, { AxiosError } from "axios";
+import { TaskGroupItem } from "../types";
+
+function useGetTasks() {
+  const url = import.meta.env.VITE_API_URL;
+  const [tasks, setTasks] = useState<TaskGroupItem[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<null | AxiosError>(null);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(url);
+        setTasks(response.data);
+      } catch (error) {
+        setError(error as AxiosError);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchTasks();
+  }, [url]);
+
+  return { tasks, loading, error };
+}
+
+export default useGetTasks;
