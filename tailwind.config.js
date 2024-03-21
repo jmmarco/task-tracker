a7be918e8588d53e60bb076922a2e6e10126e180
Juan Marco <jmmarco@gmail.com>
2024-03-21 10:08:18 +0100

    Add custom gray colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 
+const lodgify = {
+  gray: {
+    50: "#DDDDDD",
+    100: "#CCCCCC",
+    200: "#E5E5E5",
+  },
+};
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lodgify,
+      },
+    },
   },
   plugins: [],
 };
