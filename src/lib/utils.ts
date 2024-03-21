4005e2fd2f13c59437f0e4a6b1a8dd9ba87d9a7f
Juan Marco <jmmarco@gmail.com>
2024-03-21 17:59:37 +0100

    Add initializeGroups function to utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { TaskGroupItem } from "../types";
+import { GroupState } from "../reducers/groupsReducer";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function initializeGroups(taskGroups: TaskGroupItem[]): GroupState {
+  return taskGroups.map((group) => ({
+    ...group,
+    isVisible: false,
+    totalTaskValues: group.tasks.reduce((total, task) => total + task.value, 0),
+  }));
 }
