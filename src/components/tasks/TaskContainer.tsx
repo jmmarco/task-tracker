f232e01df539c71931f2de9ff15660795a75019e
Juan Marco <jmmarco@gmail.com>
2024-03-22 12:54:10 +0100

    Refactor TaskContainer component with wrapper element

--- src/components/tasks/TaskContainer.tsx
+++ src/components/tasks/TaskContainer.tsx
@@ -4,11 +4,13 @@ export default function TaskContainer({
   children: React.ReactNode;
 }) {
   return (
-    <section
-      className="mx-auto w-[820px] rounded-lg border border-lodgify-gray-100 bg-white p-4"
-      aria-label="Task List"
-    >
-      {children}
-    </section>
+    <div className="inline-block self-center bg-lodgify-gray-50 px-8 py-6">
+      <section
+        className="w-[820px] rounded-lg border border-lodgify-gray-100 bg-white p-4"
+        aria-label="Task List"
+      >
+        {children}
+      </section>
+    </div>
   );
 }
