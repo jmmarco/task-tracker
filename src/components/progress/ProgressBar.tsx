254fd4563ba146d37269dc27678bcb1aa2b33575
Juan Marco <jmmarco@gmail.com>
2024-03-22 12:25:21 +0100

    Refactor progress bar component to include ARIA attributes

--- src/components/progress/ProgressBar.tsx
+++ src/components/progress/ProgressBar.tsx
@@ -16,17 +16,17 @@ export default function ProgressBar({
   const dynamicWidth = `${value}%`;
   const displayedValue = displayValueAsInteger ? Math.floor(value) : value;
   return (
-    <div
-      className="w-full rounded-full bg-gray-200 dark:bg-gray-700"
-      aria-labelledby={name}
-      role="progressbar"
-    >
+    <div className="w-full rounded-full bg-gray-200 dark:bg-gray-700">
       <div
         className={cn(
           "flex justify-end rounded-full bg-lodgify-green-400 p-2 pr-4 text-center text-xs font-medium leading-none text-green-100 duration-300 ease-in",
           className,
         )}
+        aria-valuemin={0}
+        aria-valuemax={100}
         aria-valuenow={value}
+        role="progressbar"
+        aria-label={name}
         style={{ width: dynamicWidth }}
       >
         {displayedValue}%
