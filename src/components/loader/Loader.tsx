31a4acaac2f80cc2cd19f2d53dc8e09e75ef732d
Juan Marco <jmmarco@gmail.com>
2024-03-22 11:59:48 +0100

    Add Loader component

---
+++ src/components/loader/Loader.tsx
@@ -0,0 +1,71 @@
+interface LoaderProps {
+  loading: boolean;
+}
+
+export default function Loader({ loading }: LoaderProps) {
+  if (loading) {
+    return (
+      <div
+        className="absolute inset-0 z-50 m-auto flex items-center justify-center overflow-y-hidden bg-white opacity-80"
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <svg
+          width="44"
+          height="44"
+          viewBox="0 0 44 44"
+          xmlns="http://www.w3.org/2000/svg"
+          className=" h-24 w-24 stroke-lodgify-green-400"
+        >
+          <g fill="none" fillRule="evenodd" strokeWidth="2">
+            <circle cx="22" cy="22" r="1">
+              <animate
+                attributeName="r"
+                begin="0s"
+                dur="1.8s"
+                values="1; 20"
+                calcMode="spline"
+                keyTimes="0; 1"
+                keySplines="0.165, 0.84, 0.44, 1"
+                repeatCount="indefinite"
+              />
+              <animate
+                attributeName="stroke-opacity"
+                begin="0s"
+                dur="1.8s"
+                values="1; 0"
+                calcMode="spline"
+                keyTimes="0; 1"
+                keySplines="0.3, 0.61, 0.355, 1"
+                repeatCount="indefinite"
+              />
+            </circle>
+            <circle cx="22" cy="22" r="1">
+              <animate
+                attributeName="r"
+                begin="-0.9s"
+                dur="1.8s"
+                values="1; 20"
+                calcMode="spline"
+                keyTimes="0; 1"
+                keySplines="0.165, 0.84, 0.44, 1"
+                repeatCount="indefinite"
+              />
+              <animate
+                attributeName="stroke-opacity"
+                begin="-0.9s"
+                dur="1.8s"
+                values="1; 0"
+                calcMode="spline"
+                keyTimes="0; 1"
+                keySplines="0.3, 0.61, 0.355, 1"
+                repeatCount="indefinite"
+              />
+            </circle>
+          </g>
+        </svg>
+      </div>
+    );
+  }
+}
