c78454346c450d316f476eb9e2c9f75bf10fcd87
Juan Marco <jmmarco@gmail.com>
2024-04-10 09:53:53 +0200

    Remove custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,10 @@
 import formsPlugin from "@tailwindcss/forms";
 /** @type {import('tailwindcss').Config} */
 
-const lodgify = {
-  black: "#333333",
-  gray: {
-    50: "#EEEEEE",
-    100: "#CCCCCC",
-    200: "#E5E5E5",
-    300: "#999999",
-  },
-  green: {
-    50: "#E6FDF9",
-    400: "#02BC9C",
-  },
-};
-
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        lodgify,
-      },
-    },
+    extend: {},
   },
   plugins: [formsPlugin],
 };
